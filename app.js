c67fd3960b2e94d3cc2ada7f2244430a80e65199
Ihor Ios <osadchyi.ihor@gmail.com>
2020-01-10 00:34:40 +0200

    fixes for landing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var static = require('node-static');
-var file = new static.Server();
+var file = new static.Server({ indexFile: "index.htm" });
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
